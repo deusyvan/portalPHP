4d55b0cd9f685f0e9875d6a4e7ef5736878b0d9c
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-03-22 16:57:47 -0300

    Pintando as bolinhas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 var slideItem = 0;
+
 window.onload = function () {
+	
 	setInterval(passarSlide, 2000);
 	
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
 	var objs = document.getElementsByClassName("slide");
 	for (var i in objs){
+		if (i>-1)
 		objs[i].style.width = slidewidth;
 	}
 }
@@ -19,10 +22,12 @@ function passarSlide(){
 	}
 	
 	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+	document.getElementsByClassName("bolinha")[slideItem].style.backgroundColor = "#000";
 }
 
 function mudarSlide(pos) {
 	slideItem = pos;
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
 	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+	
 }
